6ad81e6faab198233b0ee9bf6fa76deec98a5322
jeremychipman <jchipman@walmartlabs.com>
2015-10-06 23:52:42 -0700

    Initial Commit

---
+++ Playing Around with xCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Playing Around with xCode
+//
+//  Created by Jeremy Chipman on 10/6/15.
+//  Copyright © 2015 Jeremy Chipman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
